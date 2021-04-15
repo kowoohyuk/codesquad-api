dd3b0d6191e38b8f30eefddd1b2d0e499145dd37
kowoohyuk <kowoohyuk91@gmail.com>
2021-04-15 12:17:25 +0900

    [Add] : v3
    - 조회 쿼리에서 author 삭제

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { todoRouter } from "./router/todoRouter.js";
 import { todoRouterV2 } from "./router/todoRouterV2.js";
+import { todoRouterV3 } from "./router/todoRouterV3.js";
 dotenv.config();
 const PORT = process.env.PORT;
 const app = express();
@@ -12,6 +13,8 @@ app.use(express.json());
 app.use("/todo", todoRouter);
 
 app.use("/todo/v2", todoRouterV2);
+
+app.use("/todo/v3", todoRouterV2);
 
 app.listen(PORT, function () {
   console.log(`port : ${PORT} server started, used cors`);
